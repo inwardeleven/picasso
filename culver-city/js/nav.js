873cde055a5b0ecc1c1be733ff36db50453a33bb
inwardeleven <info@inwardeleven.com>
2017-02-02 12:05:49 -0800

    track outbound links GA

--- culver-city/js/nav.js
+++ culver-city/js/nav.js
@@ -10,3 +10,11 @@ function hideNav(idname)
 {
 	document.getElementById(idname).style.color  =  "#fff";
 }
+
+function handleOutboundLinkClicks(event) {
+  ga('send', 'event', {
+    eventCategory: 'Outbound Link',
+    eventAction: 'click',
+    eventLabel: event.target.href
+  });
+}
